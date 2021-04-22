a00b0e7d4eaabd6b002729099c54186b74ff3e30
Mateus Daniel <vmateusdaniel@gmail.com>
2021-04-21 23:30:22 -0300

    Terceiro dia da nlw flutter

---
+++ lib/home/home_controller.dart
@@ -0,0 +1,39 @@
+import 'package:DevQuiz/home/home_repository.dart';
+import 'package:DevQuiz/home/home_state.dart';
+import 'package:DevQuiz/shared/models/quiz_model.dart';
+import 'package:DevQuiz/shared/models/user_model.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomeController {
+  ValueNotifier<HomeState> stateNotifier =
+      ValueNotifier<HomeState>(HomeState.empty);
+  set state(HomeState state) => stateNotifier.value = state;
+  HomeState get state => stateNotifier.value;
+
+  UserModel? user;
+  List<QuizModel>? quizzes;
+
+  final repository = HomeRepository();
+
+  void getUser() async {
+    state = HomeState.loading;
+    try {
+      user = await repository.getUser();
+      state = HomeState.success;
+    } catch (Exception) {
+      print(Exception);
+      state = HomeState.error;
+    }
+  }
+
+  void getQuizzes() async {
+    state = HomeState.loading;
+    try {
+      quizzes = await repository.getQuizzes();
+      state = HomeState.success;
+    } catch (Exception) {
+      print(Exception);
+      state = HomeState.error;
+    }
+  }
+}
